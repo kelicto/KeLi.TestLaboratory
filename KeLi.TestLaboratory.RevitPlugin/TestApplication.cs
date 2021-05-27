d5eaf01815937b0d84e4f3c5d20ba72fdba123b6
KeLi <kelicto@protonmail.com>
2021-05-27 11:16:29 +0800

    Updates code.

---
+++ KeLi.TestLaboratory.RevitPlugin/TestApplication.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.UI;
+
+namespace KeLi.TestLaboratory.RevitPlugin
+{
+    public class TestApplication : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
